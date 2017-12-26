72aff7c97c7607db2a64b408c3946f4c4388372e
AlexKv88 <32419050+AlexKv88@users.noreply.github.com>
2017-12-27 00:12:47 +0300

    Parallel

--- fragmentation.cpp
+++ fragmentation.cpp
@@ -2,15 +2,13 @@
 #include <fstream>
 
 
+
 using namespace std;
-/// вектор, содержащий box-ы, являющиеся частью рабочего пространства
-vector<Box> solution;
-/// вектор, содержащий box-ы, не являющиеся частью рабочего пространства
-vector<Box> not_solution;
-/// вектор, содержащий box-ы, находящиеся на границе между "рабочим" и "нерабочим" пространством
-vector<Box> boundary;
-/// вектор, хранящий box-ы, анализируемые на следующей итерации алгоритма
-vector<Box> temporary_boxes;
+
+cilk::reducer<cilk::op_vector<Box>> solution;
+cilk::reducer<cilk::op_vector<Box>> not_solution;
+cilk::reducer<cilk::op_vector<Box>> boundary;
+cilk::reducer<cilk::op_vector<Box>> temporary_boxes;
 
 using namespace std;
 /// функции gj()
@@ -53,7 +51,7 @@ void low_level_fragmentation::VerticalSplitter(const Box& box, boxes_pair& verti
 	double x, y, w, h;
 	box.GetParameters(x, y, w, h);
 	vertical_splitter_pair.first = Box(x, y, w / 2, h);
-	vertical_splitter_pair.second = Box(x + w / 2, y, w / 2, h);	
+	vertical_splitter_pair.second = Box(x + w / 2, y, w / 2, h);
 }
 //------------------------------------------------------------------------------------------
 void low_level_fragmentation::HorizontalSplitter(const Box& box, boxes_pair& horizontal_splitter_pair)
@@ -121,7 +119,7 @@ int low_level_fragmentation::ClasifyBox(const min_max_vectors& vects)
 	int firstL = vects.first.size();
 	int secondL = vects.second.size();
 
-	if (vects.first[0] == 0) return 3; //Граница прямоугольника
+	if (vects.first[0] == 0 && vects.second[0] == 0) return 3;  //Граница прямоугольника
 
 	for (int i = 0; i < 4; i++)
 	{
@@ -145,19 +143,19 @@ void low_level_fragmentation::GetBoxType(const Box& box)
 	type = ClasifyBox(vecs);
 
 	if (type == 0) //Убираем прямоугольник из рассмотрения
-		not_solution.push_back(box);
+		not_solution->push_back(box);
 
 	if (type == 1) //Прямоугольник, в котором есть решение (нужный экстремум)
-		solution.push_back(box);
+		solution->push_back(box);
 
 	if (type == 2) { //Прямоугольник, который надо разбить
 		GetNewBoxes(box, pair);
-		temporary_boxes.push_back(pair.first);
-		temporary_boxes.push_back(pair.second);
+		temporary_boxes->push_back(pair.first);
+		temporary_boxes->push_back(pair.second);
 	}
 
-	if (type == 3) //Пригр
-		boundary.push_back(box);
+	if (type == 3) //Граница
+		boundary->push_back(box);
 }
 //------------------------------------------------------------------------------------------
 high_level_analysis::high_level_analysis(double& min_x, double& min_y, double& x_width, double& y_height) :
@@ -240,30 +238,33 @@ void high_level_analysis::GetMinMax(const Box& box, min_max_vectors& min_max_vec
 //------------------------------------------------------------------------------------------
 void high_level_analysis::GetSolution()
 {
-	//должны фигурировать два вложенных цикла. Внешний цикл проходит по всем уровням двоичного дерева разбиения. 
-	//В рамках внутреннего цикла происходит перебор всех box-ов текущего уровня разбиения и определение типа box-а 
-	//(является он частью рабочего пространства либо не является, лежит он на границе или подлежит дальнейшему анализу). 
-
+	// Определили функцию
 	int length = FindTreeDepth() + 1;
 	boxes_pair pair;
-	temporary_boxes.push_back(current_box);
+	temporary_boxes->push_back(current_box);
 
-	for (int i = 0; i < length; i++) {
-		int number_of_box_on_level = temporary_boxes.size();
-		vector<Box> curr_boxes(temporary_boxes);
-		temporary_boxes.clear();
-		for (int j = 0; j < number_of_box_on_level; j++) {
+	for (int i = 0; i < length; i++)
+	{
+		vector<Box> tmp;
+		temporary_boxes.move_out(tmp);
+		int number_of_box_on_level = tmp.size();
+		vector<Box> curr_boxes(tmp);
+		tmp.clear();
+		temporary_boxes.set_value(tmp);
+		cilk_for(int j = 0; j < number_of_box_on_level; j++)
+		{
 			GetBoxType(curr_boxes[j]);
 		}
 	}
 }
+
 //------------------------------------------------------------------------------------------
 void WriteResults(const char* file_names[])
 {
 	// необходимо определить функцию
 	double _xmin, _xmax, _w, _h;
 	ofstream fout(file_names[0]);
-	fout << "x_min"  << " " << "y_min" << " " << "width" << " " << "height" << endl;
+	//fout << "x_min" << "\t" << "y_min" << "\t" << "width" << "\t" << "height" << endl;
 	for (int i = 0; i < solution.size(); i++) {
 		solution[i].GetParameters(_xmin, _xmax, _w, _h);
 		fout << _xmin << " " << _xmax << " " << _w << " " << _h << endl;
@@ -271,7 +272,7 @@ void WriteResults(const char* file_names[])
 	fout.close();
 
 	ofstream fout1(file_names[1]);
-	fout1 << "x_min" << "\t" << "y_min" << "\t" << "width" << "\t" << "height" << endl;
+	//fout1 << "x_min" << "\t" << "y_min" << "\t" << "width" << "\t" << "height" << endl;
 	for (int i = 0; i < not_solution.size(); i++) {
 		not_solution[i].GetParameters(_xmin, _xmax, _w, _h);
 		fout1 << _xmin << " " << _xmax << " " << _w << " " << _h << endl;
@@ -279,7 +280,7 @@ void WriteResults(const char* file_names[])
 	fout1.close();
 
 	ofstream fout2(file_names[2]);
-	fout2 << "x_min" << "\t" << "y_min" << "\t" << "width" << "\t" << "height" << endl;
+	//fout2 << "x_min" << "\t" << "y_min" << "\t" << "width" << "\t" << "height" << endl;
 	for (int i = 0; i < boundary.size(); i++) {
 		boundary[i].GetParameters(_xmin, _xmax, _w, _h);
 		fout2 << _xmin << " " << _xmax << " " << _w << " " << _h << endl;
